2e1b379e6d0a28c567975f6e313a0b8f28c3a9a0
Cezar Moreira <cezargithub@gmail.com>
2026-09-17 22:19:42 -0300

    teste3

--- index.php
+++ index.php
@@ -25,7 +25,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_POST['acao']) && $_POST['ac
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Plataforma de Serious Games - Treinamento Corporativo</title>
-    <link rel="stylesheet" href="../css/style.css">
+    <link rel="stylesheet" href="./view/css/style.css">
     
     <style>
         /* Estilos adicionais específicos para alternar abas de Acesso / Cadastro */
